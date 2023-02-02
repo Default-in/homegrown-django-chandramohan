ddc81df9e4b90f8f603c691f4186e40a62b5294a
chandramohan47 <chandramohan@getdefault.in>
2023-02-02 17:58:48 +0530

    Add songs from youtube playlist to spotify playlist

---
+++ Internet_fundamentals/youtube_to_spotify_playlist/constants.py
@@ -0,0 +1,17 @@
+GSCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+# Spotify credentials
+SPOTIFY_USERNAME = ''
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+SCOPE = 'playlist-modify-private'
+REDIRECT_URI = 'http://localhost/'
+
+# Spotify Playlist which will get the new tracks
+YOUTUBE_PLAYLIST_ID = ''
+
+API_SERVICE_NAME = "youtube"
+API_VERSION = "v3"
+CLIENT_SECRET_FILE = "client_secret.json"
+
+LOCAL_CSV_FILE = 'searched.csv'
